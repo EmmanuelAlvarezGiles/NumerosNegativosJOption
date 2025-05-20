a684183f746644af5c7daf4e0a32c7ace67b8cab
“EmmanuelAlvarezGiles” <20243rd056@utez.edu.mx>
2025-05-20 12:14:21 -0600

    El while esta en la funcion obtener numeros

--- NumerosJOption.java
+++ NumerosJOption.java
@@ -2,17 +2,20 @@ import javax.swing.JOptionPane;
 
 public class NumerosJOption{
 
-    public static int obtenerNumero(String mensaje) {
-        String input = JOptionPane.showInputDialog(mensaje);
-        return Integer.parseInt(input);
-    }
-
-    public static void main(String[] args) {
-        int numero = obtenerNumero("Ingresa números negativos:");
+    public static void obtenerNumerosNegativos() {
+        int numero = Integer.parseInt(
+            JOptionPane.showInputDialog("Ingresa un número negativo (positivo para terminar):")
+        );
 
         while (numero <= 0) {
             JOptionPane.showMessageDialog(null, "Número ingresado: " + numero);
-            numero = obtenerNumero("Ingresa otro número negativo:");
+            numero = Integer.parseInt(
+                JOptionPane.showInputDialog("Ingresa otro número negativo (positivo para terminar):")
+            );
         }
+    }
+
+    public static void main(String[] args) {
+        obtenerNumerosNegativos();
     }
 }
