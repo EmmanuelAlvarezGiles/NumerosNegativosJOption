e500e8bddaea99f9cd665101c1b0d108a65ae91c
EmmanuelAlvarezGiles <20243rd056@utez.edu.mx>
2025-05-20 12:26:08 -0600

    Update NumerosJOption.java

--- NumerosJOption.java
+++ NumerosJOption.java
@@ -2,20 +2,17 @@ import javax.swing.JOptionPane;
 
 public class NumerosJOption{
 
-    public static void obtenerNumerosNegativos() {
-        int numero = Integer.parseInt(
-            JOptionPane.showInputDialog("Ingresa un número negativo (positivo para terminar):")
-        );
+    public static int obtenerNumeros(String mensaje) {
+        int numeroTemporal= 0;
 
-        while (numero <= 0) {
-            JOptionPane.showMessageDialog(null, "Número ingresado: " + numero);
-            numero = Integer.parseInt(
-                JOptionPane.showInputDialog("Ingresa otro número negativo (positivo para terminar):")
-            );
+        while (numeroTemporal <= 0){
+            String numero = JOptionPane.showInputDialog(": ");
+            numeroTemporal= Integer.parseInt(numero);
         }
+        return numeroTemporal;
     }
 
     public static void main(String[] args) {
-        obtenerNumerosNegativos();
+        int numero = obtenerNumeros("Ingrese un numero");
     }
 }
